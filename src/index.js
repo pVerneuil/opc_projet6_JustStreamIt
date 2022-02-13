574b3f2f6445d4d5a4cb5693df2ead6e3b1b89f5
Pverneuil <verneuil.paul@gmail.com>
2022-02-13 13:00:44 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import axios from 'axios';
 
-import confetti from 'canvas-confetti';
+const urlMoviesRankedByImdbScore = 'http://localhost:8000/api/v1/titles/?sort_by=-imdb_score'
+const generateHero  = async function(url) {
+    let bestMovieUrl ;
+    try {
+        const response = await axios.get(url);
+        let moviesListRanked = await response.data.results;
+        console.log(moviesListRanked);
+        bestMovieUrl = moviesListRanked[0].url;
+    } catch (error) {
+        console.error(error);
+    }
+    let bestMovie;
+    try {
+        const response2 = await axios.get(bestMovieUrl);
+        bestMovie= await response2.data;
+        console.log(bestMovie);
+    } catch (error) {
+        console.error(error);
+    }
 
-confetti.create(document.getElementById('canvas'), {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+    document.getElementsByClassName('bestMovie__image')[0].setAttribute('src', bestMovie.image_url);
+    document.getElementsByClassName('bestMovie__title')[0].innerHTML= bestMovie.title;
+    document.getElementsByClassName('bestMovie__summary')[0].innerHTML= `<p>${bestMovie.description}<p/>`;
+};
+generateHero(urlMoviesRankedByImdbScore);
